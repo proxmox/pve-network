proxmox/pve-network

commit ba48fb02862e819552631c54b824541396a8dba6

sdn: api: route maps: fix json schema for match, set and exit-action
Add missing format_description to the value properties in match and
set property strings. Their exact format depends on the key set in the
property string.

The 2 keys that can have a value in the exit-action both refer to the
order number of a route map entry, so change the type to the
pre-defined route map order type. Order is a u16, so fix the maximum
value to match the maximum accepted value by FRR. This is wrong in
the JSON schema, but is correct in the section config type.

Reported-by: Fiona Ebner <f.ebner@proxmox.com>
Signed-off-by: Stefan Hanreich <s.hanreich@proxmox.com>
Link: https://lore.proxmox.com/20260512134336.312909-1-s.hanreich@proxmox.com

src/PVE/Network/SDN/RouteMaps.pm
@@ -48,7 +48,8 @@ our $ROUTE_MAP_MATCH_FORMAT = {
     value => {
         type => 'string',
         optional => 1,
-        description => 'value that should be matched on',
+        description => 'Value that the field <key> should be matched on.',
+        format_description => '<key-dependent>',
     },
 };
 
@@ -67,7 +68,7 @@ PVE::JSONSchema::register_standard_option(
         description => 'The index of this route map entry',
         type => 'integer',
         minimum => 0,
-        maximum => 2**32 - 1,
+        maximum => 65535,
     },
 );
 
@@ -164,7 +165,8 @@ sub route_map_properties {
                     value => {
                         type => 'string',
                         optional => 1,
-                        description => 'value that should be set to',
+                        description => 'Value that the field <key> should be set to.',
+                        format_description => '<key-dependent>',
                     },
                 },
             },
@@ -187,11 +189,7 @@ sub route_map_properties {
                         'on-match-goto', 'on-match-next', 'continue',
                     ],
                 },
-                value => {
-                    type => 'string',
-                    optional => 1,
-                    description => 'type of exit action',
-                },
+                value => get_standard_option('pve-sdn-route-map-order'),
             },
             optional => 1,
         },

commit 92e2fd055e4b4291f9affcfd118c3103c56d6222

api: route maps: make value property optional for exit-action
'on-match-next' doesn't require a value to be set, so make the
property optional again - as it was before changing the type to the
pve-sdn-route-map-order option.

Fixes: ba48fb0 ("sdn: api: route maps: fix json schema for match, set and exit-action")
Signed-off-by: Stefan Hanreich <s.hanreich@proxmox.com>
Link: https://lore.proxmox.com/20260513114855.163214-1-s.hanreich@proxmox.com
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/Network/SDN/RouteMaps.pm
@@ -189,7 +189,9 @@ sub route_map_properties {
                         'on-match-goto', 'on-match-next', 'continue',
                     ],
                 },
-                value => get_standard_option('pve-sdn-route-map-order'),
+                value => get_standard_option('pve-sdn-route-map-order', {
+                        optional => 1,
+                }),
             },
             optional => 1,
         },